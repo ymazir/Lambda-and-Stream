cb63d3405861955fb954a7b7e5d1cbe12fd214c3
Maicheal Shenouda <rory1980rory1980@gmail.com>
2025-10-22 11:53:24 -0400

    Created groupAdventurersByRoles method in AdventurerExample.java. This method groups all adventurers across all guilds by their role and returns a map as well as prints them.

--- src/AdventurerExample.java
+++ src/AdventurerExample.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 
@@ -7,22 +8,29 @@ public class AdventurerExample {
 
     ArrayList<Guild> guildList = new ArrayList<>();
 
-    Adventurer adventurer1 = new Adventurer("Arin", 25, Role.Warrior, 1500.0, List.of(Skill.SWORDSMANSHIP, Skill.HORSEMANSHIP));
-    Adventurer adventurer2 = new Adventurer("Lina", 22, Role.Rouge, 1200.0, List.of(Skill.ARCHERY, Skill.STEALTH));
-    Adventurer adventurer3 = new Adventurer("random", 20, Role.Rouge, 1000.0, List.of(Skill.THIEVERY, Skill.STEALTH));
-    Adventurer adventurer4 = new Adventurer("Mira", 30, Role.Wizard, 2000.0, List.of(Skill.NECROMANCY, Skill.RUNECRAFTING));
-    Adventurer adventurer5 = new Adventurer("Doran", 28, Role.Warrior, 1800.0, List.of(Skill.SWORDSMANSHIP, Skill.BLACKSMITHING));
-    Adventurer adventurer6 = new Adventurer("Mike", 20, Role.Rouge, 1000000000.0, List.of(Skill.HEALING, Skill.BLACKSMITHING, Skill.HORSEMANSHIP, Skill.ARCHERY, Skill.THIEVERY, Skill.MEMECRAFTING, Skill.NECROMANCY, Skill.STEALTH, Skill.RUNECRAFTING, Skill.SWORDSMANSHIP));
-
-
-    Guild guild1 = new Guild("test", List.of(adventurer1, adventurer2, adventurer3));
-    Guild guild2 = new Guild("testAgainlol", List.of(adventurer4, adventurer5, adventurer6));
-
-
     public static List<Adventurer> filterAdventurers(List<Guild> guilds, Skill skillToFilter) {
         return guilds.stream()
                 .flatMap(m -> m.getAdventureres().stream())
                 .filter(p -> p.getSkills().contains(skillToFilter))
                 .collect(Collectors.toList());
+    }
+
+    public static Map<Role, List<Adventurer>> groupAdventurersByRoles(List<Guild> guilds) {
+        Map<Role, List<Adventurer>> groups = guilds.stream()
+                .flatMap(m -> m.getAdventureres().stream())
+                .collect(Collectors.groupingBy(Adventurer::getRole));
+
+
+
+        //prints anyways and returns the whole map. comment this out more tonight
+        for (Role role : groups.keySet()) {
+            System.out.println("Role: " + role);
+            for (Adventurer adventurer : groups.get(role)) {
+                System.out.println(" - " + adventurer.getName());
+            }
+        }
+
+
+        return groups;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Adventurer adventurer1 = new Adventurer("Arin", 25, Role.Warrior, 1500.0, List.of(Skill.SWORDSMANSHIP, Skill.HORSEMANSHIP));
+        Adventurer adventurer2 = new Adventurer("Lina", 22, Role.Rouge, 1200.0, List.of(Skill.ARCHERY, Skill.STEALTH));
+        Adventurer adventurer3 = new Adventurer("random", 20, Role.Rouge, 1000.0, List.of(Skill.THIEVERY, Skill.STEALTH));
+        Adventurer adventurer4 = new Adventurer("Mira", 30, Role.Wizard, 2000.0, List.of(Skill.NECROMANCY, Skill.RUNECRAFTING));
+        Adventurer adventurer5 = new Adventurer("Doran", 28, Role.Warrior, 1800.0, List.of(Skill.SWORDSMANSHIP, Skill.BLACKSMITHING));
+        Adventurer adventurer6 = new Adventurer("Mike", 20, Role.Rouge, 1000000000.0, List.of(Skill.HEALING, Skill.BLACKSMITHING, Skill.HORSEMANSHIP, Skill.ARCHERY, Skill.THIEVERY, Skill.MEMECRAFTING, Skill.NECROMANCY, Skill.STEALTH, Skill.RUNECRAFTING, Skill.SWORDSMANSHIP));
+
+
+        Guild guild1 = new Guild("test", List.of(adventurer1, adventurer2, adventurer3));
+        Guild guild2 = new Guild("testAgainlol", List.of(adventurer4, adventurer5, adventurer6));
+
+
+        System.out.println(AdventurerExample.filterAdventurers(List.of(guild1, guild2), Skill.BLACKSMITHING));
+
+        System.out.println(AdventurerExample.groupAdventurersByRoles(List.of(guild1, guild2)));
     }
 }
