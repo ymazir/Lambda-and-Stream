94082e1097eddfccf5b104affc17f0daa9d35094
Maicheal Shenouda <rory1980rory1980@gmail.com>
2025-10-31 16:36:42 -0400

    Implemented all the methods assigned using mostly streams and lambdas. Demonstating functionality can be found in Main.java. All the methods were implemented in AdventurerExample.java.

--- src/AdventurerExample.java
+++ src/AdventurerExample.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -20,8 +18,6 @@ public class AdventurerExample {
                 .flatMap(m -> m.getAdventureres().stream())
                 .collect(Collectors.groupingBy(Adventurer::getRole));
 
-
-
         //prints anyways and returns the whole map. comment this out more tonight
         for (Role role : groups.keySet()) {
             System.out.println("Role: " + role);
@@ -33,4 +29,52 @@ public class AdventurerExample {
 
         return groups;
     }
+
+    public static Adventurer getMostSkilledAdventurer(List<Guild> guilds) {
+        return guilds.stream()
+                .flatMap(m -> m.getAdventureres().stream())
+                .max(Comparator.comparingInt(a -> a.getSkills().size()))
+                .get();
+    }
+
+
+    // some bullshit bro holy moly it makes a Map of guilds to integers and returns it after its soorted.
+    public static List<Guild> rankGuildsByAverageAdventurerAge(List<Guild> guilds) {
+
+        return guilds.stream()                                           // Starts the stream
+                .sorted(Comparator                                       // Sorts the stream by a comparator were about to give it
+                        .comparingDouble(g -> g.getAdventureres().stream() // Comparing by double, but first get reach guild and make a stream of all of its adventurers
+                        .mapToInt(Adventurer::getAge)                    // Maps each adventurer to their age as an int
+                        .average()                                       // Gets the average of the ages in the stream
+                        .orElse(0)))                               // The whole thing breaks if this isnt here fuck this stupid thing bro its just a default to 0
+                .collect(Collectors.toList());                           // Collects the sorted stream of guilds into a new list then returns
+
+    }
+
+    public static Map<String, Long> skillWiseAdventurerCount(List<Guild> guilds) {
+
+        Map<String, Long> temp = guilds.stream()
+                .flatMap(g -> g.getAdventureres().stream())
+                .flatMap(a -> a.getSkills().stream())
+                .collect(Collectors.groupingBy(Skill::name, Collectors.counting()));
+
+        for (Map.Entry entry : temp.entrySet() ) {
+            System.out.println(entry.getKey() + ", " + entry.getValue());
+        }
+
+
+        return temp;
+    }
+
+    public static void goldBonusEvent(List<Guild> guilds) {
+       guilds.stream().flatMap(g -> g.getAdventureres().stream()).filter(a -> a.getGoldEarned() < 1000).forEach(adventurer -> adventurer.setGoldEarned(adventurer.getGoldEarned()*1.2));
+
+       guilds.stream().flatMap(g -> g.getAdventureres().stream()).forEach(adventurer -> System.out.println(adventurer.getName() + ": " + adventurer.getGoldEarned()));
+    }
+
+
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
 
         Adventurer adventurer1 = new Adventurer("Arin", 25, Role.Warrior, 1500.0, List.of(Skill.SWORDSMANSHIP, Skill.HORSEMANSHIP));
-        Adventurer adventurer2 = new Adventurer("Lina", 22, Role.Rouge, 1200.0, List.of(Skill.ARCHERY, Skill.STEALTH));
-        Adventurer adventurer3 = new Adventurer("random", 20, Role.Rouge, 1000.0, List.of(Skill.THIEVERY, Skill.STEALTH));
+        Adventurer adventurer2 = new Adventurer("Lina", 232, Role.Rouge, 1200.0, List.of(Skill.ARCHERY, Skill.STEALTH));
+        Adventurer adventurer3 = new Adventurer("random", 29000, Role.Rouge, 999.0, List.of(Skill.THIEVERY, Skill.STEALTH));
         Adventurer adventurer4 = new Adventurer("Mira", 30, Role.Wizard, 2000.0, List.of(Skill.NECROMANCY, Skill.RUNECRAFTING));
         Adventurer adventurer5 = new Adventurer("Doran", 28, Role.Warrior, 1800.0, List.of(Skill.SWORDSMANSHIP, Skill.BLACKSMITHING));
-        Adventurer adventurer6 = new Adventurer("Mike", 20, Role.Rouge, 1000000000.0, List.of(Skill.HEALING, Skill.BLACKSMITHING, Skill.HORSEMANSHIP, Skill.ARCHERY, Skill.THIEVERY, Skill.MEMECRAFTING, Skill.NECROMANCY, Skill.STEALTH, Skill.RUNECRAFTING, Skill.SWORDSMANSHIP));
+        Adventurer adventurer6 = new Adventurer("Mike", 205, Role.Rouge, 1000000000.0, List.of(Skill.HEALING, Skill.BLACKSMITHING, Skill.HORSEMANSHIP, Skill.ARCHERY, Skill.THIEVERY, Skill.MEMECRAFTING, Skill.NECROMANCY, Skill.STEALTH, Skill.RUNECRAFTING, Skill.SWORDSMANSHIP));
 
 
         Guild guild1 = new Guild("test", List.of(adventurer1, adventurer2, adventurer3));
@@ -19,5 +20,29 @@ public class Main {
         System.out.println(AdventurerExample.filterAdventurers(List.of(guild1, guild2), Skill.BLACKSMITHING));
 
         System.out.println(AdventurerExample.groupAdventurersByRoles(List.of(guild1, guild2)));
+
+        System.out.println(AdventurerExample.getMostSkilledAdventurer(List.of(guild1, guild2)).getName());
+
+        List<Guild> sortedByAverageAgeListGuilds = AdventurerExample.rankGuildsByAverageAdventurerAge(List.of(guild1, guild2));
+
+        for (Guild guild : sortedByAverageAgeListGuilds) {
+
+            System.out.println(guild + " " + getAverageAge(guild));
+        }
+
+
+        System.out.println(AdventurerExample.skillWiseAdventurerCount(List.of(guild1, guild2)));
+
+        AdventurerExample.goldBonusEvent(List.of(guild1, guild2));
+
+    }
+
+    // Helper method to demonstrate functionality of AdventurerExample.rankGuildsByAverageAdventurerAge
+    public static int getAverageAge(Guild guild) {
+        int counter = 0;
+        for (Adventurer adv : guild.getAdventureres()) {
+            counter += adv.getAge();
+        }
+        return counter / guild.getAdventureres().size();
     }
 }
